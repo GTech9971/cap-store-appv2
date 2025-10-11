0b7fba6653d4d0655d254828e0aee1a1096190cc
George <georgetech1984@gmail.com>
2025-10-12 00:44:50 +0900

    外部リンクコンポーネント作成

---
+++ packages/ui/src/components/external-link/ExternalLinkCard.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { ExternalLinkCard } from './ExternalLinkCard';
+
+
+const meta: Meta<typeof ExternalLinkCard> = {
+    title: 'Components/ExternalLinkCard',
+    component: ExternalLinkCard,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+
+} satisfies Meta<typeof ExternalLinkCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        link: 'https://github.com/GTech9971/CapStore-api-definition',
+        onEditedLink: (link) => console.warn(link),
+        title: 'Github',
+        onEditedTitle: (title) => console.warn(title),
+        tag: 'Github',
+        onEditedTag: (tag) => console.warn(tag)
+    },
+};
